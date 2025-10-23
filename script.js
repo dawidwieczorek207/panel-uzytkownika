8003b40e421db6eb631b85b22c61067c23400754
Dawid <dawidwieczorek207@gmail.com>
2025-10-23 13:13:03 +0200

    Dzień 2: frontend logowania i panelu

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Dzień 1 — wszystko działa!");
+const loginForm = document.getElementById("login-form");
+const registerForm = document.getElementById("register-form");
+const showRegister = document.getElementById("show-register");
+const showLogin = document.getElementById("show-login");
+
+showRegister.addEventListener("click", () => {
+  loginForm.classList.remove("active");
+  registerForm.classList.add("active");
+});
+
+showLogin.addEventListener("click", () => {
+  registerForm.classList.remove("active");
+  loginForm.classList.add("active");
+});
+
+// tymczasowe symulacje logowania
+document.getElementById("login-btn").addEventListener("click", () => {
+  window.location.href = "dashboard.html";
+});
